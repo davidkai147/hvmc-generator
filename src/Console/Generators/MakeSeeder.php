af96e9b64aa2e65ada80121361a8b2cd7d59b7f7
Cong Do <dochauthanhcong147@gmail.com>
2021-05-06 18:45:51 +0700

    Create make seeder

---
+++ src/Console/Generators/MakeSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace HMVCTools\Console\Generators;
+
+use Symfony\Component\Process\Process;
+
+
+class MakeSeeder extends AbstractGenerator
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'module:make:seed
+    	{module : The alias of the module}
+    	{name : The class name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new seeder for the specified module.';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Seeder';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub(): string
+    {
+        return __DIR__ . '/partial_stubs/seeder.stub';
+    }
+
+    /**
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function handle(): void
+    {
+        parent::handle();
+        $process = Process::fromShellCommandline('composer du');
+        $process->run();
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function getPath($name): string
+    {
+        return $this->modulePath() . 'database/seeders/' . str_replace('\\', '/', $name) . '.php';
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    protected function getClass(string $name): string
+    {
+        return $name;
+    }
+}
